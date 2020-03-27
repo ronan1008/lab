e1f8420487193d049d8d7b213a3560ad5e2923f6
ronan1008 <57070816+ronan1008@users.noreply.github.com>
2020-03-28 01:19:14 +0800

    sms minor

--- Python/Ace/Android/tools/smsParser.py
+++ Python/Ace/Android/tools/smsParser.py
@@ -1,10 +1,8 @@
 from pyquery import PyQuery as pq
-import requests
+import requests , pprint , re
 import pprint
-from IPython.core.display import display, HTML
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'}
 r = requests.get('https://smsreceivefree.com/info/18192724632/',headers=headers)
-print(r.status_code)
 doc = pq(r.text)
 td = doc("td")
 smsList=[]
@@ -18,7 +16,26 @@ for n , item in enumerate(td.items()):
         smsStr += newStr
         smsList.append(smsStr)
         smsStr=''
-for i in smsList:
-    print(i+'\n')
-#pprint.pprint(smsList)
-#14249001XXX	20 seconds ago	[ACE]Verification code: 306692, Available for 10 minutes. Please check that you are visiting "ace.io", and do not share the code with anyone!
+
+def find_verifi_code(items):
+    match = re.search(r"Verification code: (\d+)", items)
+    if match :
+        verificationCode = match.group(1)
+        return verificationCode
+    else:
+        return False
+
+def get_verification_code(pattern):
+    for i in smsList:
+        iList = i.split(',')
+        if iList[1].find('seconds') != -1 and iList[2].find(pattern) !=-1 :
+            find_verifi_code(iList[2])
+        elif iList[1].find('minutes') != -1 and iList[2].find(pattern) !=-1 :
+            find_verifi_code(iList[2])
+
+
+print(get_verification_code('[ACE]'))
+
+#14249001XXX	20 seconds ago	[ACE]Verification code: 306692, Available for 10 minutes. Please check that you are visiting "ace.io", and do not share the code with anyone !
+
+
