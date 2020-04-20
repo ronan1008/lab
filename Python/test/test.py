b93e5a10a2d3a1fc39c1534b4748faed34aa1965
LEE CHENG HAN <shocklee@Shock-MacBook-Pro.local>
2020-04-20 22:22:11 +0800

    minor fixed

--- Python/test/test.py
+++ Python/test/test.py
@@ -1,12 +1,13 @@
-import requests
+import urllib.parse
 
-url = "https://stage.ace.io/polarisex/open/v1/coin/customerAccount"
 
-payload = 'uid=437&timeStamp=1587376807000&signKey=0fbd19ed427355b5bda40145c02b80aa&apiKey=437%232020&securityKey=50caded91f924ed184ce173177294b15'
-headers = {
-  'Content-Type': 'application/x-www-form-urlencoded'
+data = {
+        'uid': 123123,
+        'timeStamp': 4121242300,
+        'signKey': 'sfsdflksjdflksjdlf',
+        'apiKey' : 'fsdfsdfs',
+        'securityKey' : 'fsdfsdf',
 }
+data = urllib.parse.urlencode(data)
 
-response = requests.request("POST", url, headers=headers, data = payload)
-
-print(response.text.encode('utf8'))
+print(data)
--- Python/test/test_ace_api.py
+++ Python/test/test_ace_api.py
@@ -3,14 +3,13 @@ import datetime
 import requests
 import json
 import urllib.parse
-
+from pprint import pprint
 
 def generate_signKey(phone_num, timestamp, ace_sign = 'ACE_SIGN'):
     data = ace_sign + str(timestamp) + phone_num
     md5 = hashlib.md5()
     md5.update(data.encode("utf-8"))
     signKey = md5.hexdigest()
-    print(data)
     return signKey
 
 def post_api(data, api_url):
@@ -18,7 +17,9 @@ def post_api(data, api_url):
         'Content-Type': 'application/x-www-form-urlencoded'
     }
     response = requests.request("POST", api_url, headers=headers, data = data)
-    print(response.text)
+    obj = json.loads(response.text)
+    pprint(obj)
+
 
 
 if __name__ == '__main__':
@@ -32,17 +33,28 @@ if __name__ == '__main__':
 #    api_url = test_site
     uid = '437'
     apiKey = "437#2020"
-    securityKey = "50caded91f924ed184ce173177294b15" 
+    securityKey = "50caded91f924ed184ce173177294b15"
 
     phone_num = '0886936736561'
     timestamp = int(datetime.datetime.now().timestamp())
     format_timestamp = str('{:0<13d}'.format(timestamp))
     signKey = generate_signKey(phone_num, format_timestamp)
     format_timestamp = urllib.parse.quote_plus(format_timestamp)
-    data = "uid={}&timeStamp={}&signKey={}&apiKey={}&securityKey={}".format(uid, format_timestamp, signKey, apiKey, securityKey)
+
+    data = {
+        'uid': uid,
+        'timeStamp': format_timestamp,
+        'signKey': signKey,
+        'apiKey' : apiKey,
+        'securityKey' : securityKey
+    }
+
+    data = urllib.parse.urlencode(data)
+
+#    data = "uid={}&timeStamp={}&signKey={}&apiKey={}&securityKey={}".format(uid, format_timestamp, signKey, apiKey, securityKey)
     post_api(data, api_url)
 
-    print('uid', uid) 
+    print('uid', uid)
     print('timeStamp', format_timestamp)
     print('signKey', signKey)
     print('apiKey', apiKey)
