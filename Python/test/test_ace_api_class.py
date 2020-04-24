0743a6918e87767b6b5b30696a89796d343ca9c5
Shock Lee <ronan1008@yahoo.com.tw>
2020-04-24 18:23:02 +0800

    add new attributes data

--- Python/test/test_ace_api_class.py
+++ Python/test/test_ace_api_class.py
@@ -19,7 +19,7 @@ class Ace:
         self.timestamp = self._ace_format_timestamp()
         self.signKey = self._generate_signKey()
         self.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-
+        self.data = None
         #dictionary
         self.currency = { 'TWD':1, 'BTC':2, 'ETH':4, 'LTC':7, 'XRP':10, 'TRX':13, 'USDT':14, 'BNB':17, 'BTT':19, }
         self.buy_or_sell = { 'buy':1, 'sell':2, }
@@ -50,7 +50,7 @@ class Ace:
     def customer_account_info(self):
         partial_url = '/coin/customerAccount'
 
-        data = {
+        self.data = {
             'uid': self.uid,
             'timeStamp': self.timestamp,
             'signKey': self.signKey,
@@ -58,25 +58,25 @@ class Ace:
             'securityKey' : self.securityKey
         }
 
-        obj = self._post_api(data, partial_url)
+        obj = self._post_api(self.data, partial_url)
         return obj
 
     def get_coin_exchange_info(self):
         partial_url = '/coin/coinRelations'
 
-        data = {
+        self.data = {
             'uid': self.uid,
             'timeStamp': self.timestamp,
             'signKey': self.signKey,
             'apiKey' : self.apiKey,
             'securityKey' : self.securityKey
         }
-        obj = self._post_api(data, partial_url)
+        obj = self._post_api(self.data, partial_url)
         return obj
 
     def get_kline_info(self,tradeCurrencyId, baseCurrencyId, limit):
         partial_url = '/kline/getKlineMin'
-        data = {
+        self.data = {
             'uid': self.uid,
             'timeStamp': self.timestamp,
             'signKey': self.signKey,
@@ -86,13 +86,13 @@ class Ace:
             'baseCurrencyId' : baseCurrencyId,
             'limit' : limit,
         }
-        obj = self._post_api(data, partial_url)
+        obj = self._post_api(self.data, partial_url)
         return obj
 
 
     def new_order(self, currencyId, baseCurrencyId, buyOrSell, price, num, trade_type):
         partial_url = '/order/order'
-        data = {
+        self.data = {
             'uid': self.uid,
             'timeStamp': self.timestamp,
             'signKey': self.signKey,
@@ -106,14 +106,14 @@ class Ace:
             'type' : self.order_type[trade_type],
             'fdPassword' : None,
         }
-        obj = self._post_api(data, partial_url)
+        obj = self._post_api(self.data, partial_url)
         return "Create order "+obj['attachment']+" Success!!" if obj['status']== 200 else  obj['message']
 
 
     def cancel_order(self, orderNo):
         #print("process: {} start".format(orderNo))
         partial_url = '/order/cancel'
-        data = {
+        self.data = {
             'uid': self.uid,
             'timeStamp': self.timestamp,
             'signKey': self.signKey,
@@ -122,14 +122,14 @@ class Ace:
             'orderNo': orderNo,
         }
 
-        obj = self._post_api(data, partial_url)
+        obj = self._post_api(self.data, partial_url)
         return "Cancel "+orderNo+" Success!!" if obj['status']== 200 else  obj['message']
 
 
 
     def get_order_list(self):
         partial_url = '/order/getOrderList'
-        data = {
+        self.data = {
             'uid': self.uid,
             'timeStamp': self.timestamp,
             'signKey': self.signKey,
@@ -140,12 +140,12 @@ class Ace:
             # 'start' : None,
             'size' : 100,
         }
-        obj = self._post_api(data, partial_url)
+        obj = self._post_api(self.data, partial_url)
         return obj
 
     def show_order_status(self, orderId):
         partial_url = '/order/showOrderStatus'
-        data = {
+        self.data = {
             'uid': self.uid,
             'timeStamp': self.timestamp,
             'signKey': self.signKey,
@@ -153,12 +153,12 @@ class Ace:
             'securityKey' : self.securityKey,
             'orderId' : orderId,
         }
-        obj = self._post_api(data, partial_url)
+        obj = self._post_api(self.data, partial_url)
         return obj
 
     def show_order_history(self, orderId):
         partial_url = '/order/showOrderHistory'
-        data = {
+        self.data = {
             'uid': self.uid,
             'timeStamp': self.timestamp,
             'signKey': self.signKey,
@@ -166,7 +166,7 @@ class Ace:
             'securityKey' : self.securityKey,
             'orderId' : orderId,
         }
-        obj = self._post_api(data, partial_url)
+        obj = self._post_api(self.data, partial_url)
         return obj
 
 
@@ -211,8 +211,9 @@ class Ace:
 if __name__ == '__main__':
     #uid, apiKey, securityKey, phone_num
     member_a = Ace(437, "437#2020", "50caded91f924ed184ce173177294b15", '0886936736561')
-    #account_info = member_a.customer_account_info()
-    #pprint(account_info)
+    account_info = member_a.customer_account_info()
+    pprint(account_info)
+    pprint(member_a.data)
 
     #使用 TWD 買入BTC,以 現價委託 193346.9的價格買入數量 0.001 
     #order_status = member_a.new_order('BTC', 'TWD', 'buy', "193346.9" , "0.001", 'limit')
@@ -232,5 +233,5 @@ if __name__ == '__main__':
 
 
     #查看該筆訂單歷史紀錄
-    order_status = member_a.show_order_history('15875352484822652103401100243963')
-    pprint(order_status)
+    #order_status = member_a.show_order_history('15875352484822652103401100243963')
+    #pprint(order_status)
