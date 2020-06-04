c2fcf1eae5e5962b0a76058b7d6289689febb13e
ronan1008 <57070816+ronan1008@users.noreply.github.com>
2020-06-04 22:53:04 +0800

    test

--- Node/clean/controllers/getPost.js
+++
@@ -1,8 +0,0 @@
-const BlogPost = require('../models/BlogPost.js')
-module.exports = async(req,res)=>{
-    const blogpost = await BlogPost.findById(req.params.id)
-    console.log(blogpost)
-    res.render('post',{
-       blogpost 
-    });
-}
