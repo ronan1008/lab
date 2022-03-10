90614d4bffe403823f3c31cdeb016b37925f3344
shock <ronan1008@yahoo.com.tw>
2022-03-10 17:15:28 +0800

    ptt spiders use items

--- Python/tempTest/myFirst/myFirst/items.py
+++ Python/tempTest/myFirst/myFirst/items.py
@@ -9,4 +9,9 @@ import scrapy
 class MyfirstItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author = scrapy.Field()
+    push = scrapy.Field()
+    href = scrapy.Field()
+    date = scrapy.Field()
+
--- Python/tempTest/myFirst/myFirst/spiders/example.py
+++ Python/tempTest/myFirst/myFirst/spiders/example.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy.exceptions import CloseSpider
+from ..items import MyfirstItem
 
 class PttSpider(scrapy.Spider):
     count_page = 1
@@ -7,15 +8,14 @@ class PttSpider(scrapy.Spider):
     allowed_domains = ['www.ptt.cc']
     start_urls = ['https://www.ptt.cc/bbs/movie/index.html']
     def parse(self, response):
+        items = MyfirstItem()
         for q in response.css('div.r-ent'):
-            item = {
-                'push':q.css('div.nrec > span.hl::text').extract_first(),
-                'title':q.css('div.title > a::text').extract_first(),
-                'href':q.css('div.title > a::attr(href)').extract_first(),
-                'date':q.css('div.meta > div.date::text').extract_first(),
-                'author':q.css('div.meta > div.author::text').extract_first()
-            }
-            yield(item)
+            items['push'] = q.css('div.nrec > span.hl::text').extract_first()
+            items['title'] = q.css('div.title > a::text').extract_first()
+            items['href'] = q.css('div.title > a::attr(href)').extract_first()
+            items['date'] = q.css('div.meta > div.date::text').extract_first()
+            items['author'] = q.css('div.meta > div.author::text').extract_first()
+            yield(items)
         next_page_url = response.css('div.action-bar > div.btn-group > a.btn::attr(href)')[3].extract()
         print(f"{next_page_url}, {self.count_page} .............")
         if next_page_url and self.count_page <= 10:
