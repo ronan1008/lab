9e08c7b42b6c76ca7f7bdbd358fff987591794c4
Shock Lee <ronan1008@yahoo.com.tw>
2022-08-15 09:40:03 +0000

    socket connect

--- Python/socket_test/tcp_socket_client.py
+++ Python/socket_test/tcp_socket_client.py
@@ -1,1 +1,11 @@
-print('test sample')
+import socket
+
+def main():
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server_socket.bind(('', 8000))
+    tcp_server_socket.listen(128)
+    client_socket, clientAddr =  tcp_server_socket.accept()
+
+
+if __name__ == '__main__':
+    main()
