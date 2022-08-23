20f83f7064f08568e34804a3dd81548f5ecc3f2c
Shock Lee <ronan1008@yahoo.com.tw>
2022-08-23 09:34:13 +0000

    practice basic one tcp server

--- Python/socket_test/tcp_socket_server.py
+++ Python/socket_test/tcp_socket_server.py
@@ -4,8 +4,24 @@ def main():
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_server_socket.bind(('', 8000))
     tcp_server_socket.listen(128)
-    client_socket, clientAddr =  tcp_server_socket.accept()
 
+    while True:
+        print("waiting a client......")
+        client_socket, clientAddr =  tcp_server_socket.accept()
+        print("a client is coming...")
+        print(clientAddr)
+        while True:
+            print("waiting client ask...")
+            recv_data  = client_socket.recv(1024)
+            print(recv_data.decode('utf-8'))
+            print("sending to client")
+
+            if recv_data:
+                client_socket.send(" hahaha ---ok---".encode("utf-8"))
+            else:
+                break
+        client_socket.close()
+    tcp_server_socket.close()
 
 if __name__ == '__main__':
     main()
