007efcfdaa8c3db04aeddf21d4d93ba0b0f0171c
shock <ronan1008@yahoo.com.tw>
2022-11-14 23:00:45 +0800

    port 80 to 8088

--- php/docker-php-apache/www/esg_sample/order.php
+++ php/docker-php-apache/www/esg_sample/order.php
@@ -17,23 +17,23 @@
 	// 商家遊戲代碼
 	$trans->nodes["CID"] = "C000150000015";
 	// 商家訂單編號
-	$trans->nodes["COID"] = "CP" . date("YmdHis");
+	$trans->nodes["COID"] = "es2211145gkBwE6ZVeRk";
 	// 幣別 ISO Alpha Code
 	$trans->nodes["CUID"] = "VND";
 	// 付款代收業者代碼 
 	$trans->nodes["PAID"] = "";
 	// 交易金額
-	$trans->nodes["AMOUNT"] = "10000.00";
+	$trans->nodes["AMOUNT"] = "100000.0000";
 	// 商家接收交易結果網址
-	$trans->nodes["RETURN_URL"] = "http://xxx.xxx.xxx.xxx/Return_URL.php";
+	$trans->nodes["RETURN_URL"] = "https://ptsv2.com/t/gpa6a-1668410593/post";
 	// 是否指定付款代收業者
 	$trans->nodes["ORDER_TYPE"] = "E"; // 請固定填 E 
 	// 商家代碼
 	$trans->nodes["MID"] = "M3000015";
 	// 商家商品名稱
-	$trans->nodes["PRODUCT_NAME"] = "Lễ bao 10K";
+	$trans->nodes["PRODUCT_NAME"] = "Esgame點數 240000";
 	// 商家商品代碼
-	$trans->nodes["PRODUCT_ID"] = "vn.esgame.lh3d_e_lb_00010";
+	$trans->nodes["PRODUCT_ID"] = "xtars.web.esgame.points.240000";
 	// 玩家帳號
 	$trans->nodes["USER_ACCTID"] = "40";
 	// 交易備註 ( 此為選填 )
@@ -51,8 +51,21 @@
 	
 	// 取得送出之交易資料
 	$data = $trans->GetSendData();
-	echo $data
+	$data = "eyJNU0dfVFlQRSI6ICIwMTAwIiwgIlBDT0RFIjogIjMwMDAwMCIsICJDSUQiOiAiQzAwMDE1MDAwMDAxNSIsICJDVUlEIjogIlZORCIsICJQQUlEIjogIiIsICJBTU9VTlQiOiAxMDAwMDAuMCwgIlVTRVJfQUNDVElEIjogIjc3NGFiNzNmLTZhYTYtNGVkMS1hOTkxLTYwOWQ3ZGI3ZDFhMyIsICJSRVRVUk5fVVJMIjogImh0dHBzOi8vcHRzdjIuY29tL3QvZ3BhNmEtMTY2ODQxMDU5My9wb3N0IiwgIk9SREVSX1RZUEUiOiAiRSIsICJNSUQiOiAiTTMwMDAwMTUiLCAiUFJPRFVDVF9OQU1FIjogIkVzZ2FtZVx1OWVkZVx1NjU3OCAyNDAwMDAiLCAiUFJPRFVDVF9JRCI6ICJ4dGFycy53ZWIuZXNnYW1lLnBvaW50cy4yNDAwMDAiLCAiQ09JRCI6ICJlczIyMTExNDVSUE5OVGFUbzNuaiIsICJFUlFDIjogImxLcXFrUzcrNzVvM1dadlRubXRWbXJjVFVBbz0iLCAiRVJRQ0lJIjogIkg0bmJyTjR3dDNOUHNrbTdEMnJJTm1kZTErUT0ifQ==";
 	
+   
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_POST['fname'];
+        if (empty($name)) {
+            echo "Name is empty";
+        } else {
+            echo $name;
+        }
+    }
+ 
+
+    echo $name;
 ?>
 
 <html> 
@@ -66,7 +79,7 @@
 	COID : <?php echo $trans->nodes["COID"]; ?>
 	
 	<form name="form1" id="form1" action="https://pay.esgame.vn/purchase/3rd/generate-purchase" method="post" target="_blank">
-	<input type="hidden" name="data" value="<?php echo $data ?>">
+	<input type="hidden" name="data" value="<?php echo $name ?>">
 	<input type="submit" value="送出交易至ESG測試機">
 	</form>
 	
